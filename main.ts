1ae5f514673b443508c800a3e2d529a7afb7ad57
micheleboufim-a11y <micheleboufim@gmail.com>
2025-11-02 15:05:00 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # . # # .
+    # . . # .
+    # # # # .
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . # .
+    # . . # .
+    # . . # .
+    # # # # .
+    `)
